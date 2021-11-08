59fc7f883e63df66b326063289ce0741a855aacd
Mohammad Azam <azamsharp@gmail.com>
2021-11-08 08:19:10 -0600

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Mohammad Azam on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BudgetApp
+//
+//  Created by Mohammad Azam on 11/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
